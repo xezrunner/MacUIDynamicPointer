3aeb96de555a4fdb61260aa071b7b8e050785932
xezrunner <8061077+xezrunner@users.noreply.github.com>
2025-05-23 17:21:51 +0200

    Initial Commit

---
+++ MacUIDynamicPointer/MacUIDynamicPointerApp.swift
@@ -0,0 +1,12 @@
+// MacUIDynamicPointer::MacUIDynamicPointerApp.swift - 23.05.2025
+
+import SwiftUI
+
+@main
+struct MacUIDynamicPointerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
